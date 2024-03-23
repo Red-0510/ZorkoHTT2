62e166889c1b75ed18db10aaf7ddb66841ef30ea
Red-0510 <jaineshmachhi@gmail.com>
2024-03-23 13:43:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/zorkohtt2/Start_Activity.java
@@ -0,0 +1,14 @@
+package com.example.zorkohtt2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Start_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
